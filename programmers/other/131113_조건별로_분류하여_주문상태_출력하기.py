3d5f2aeec4b0ad5b4dc2cd0550a4c76de5a2aa98
Sunyeong Kim <iloveyou7032@gmail.com>
2026-01-19 09:45:30 +0900

    [프로그래머스] 조건별로 분류하여 주문상태 출력하기 풀이 제출
    - 문제 ID: 131113
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 09:45:29
    - 작성자: 김선영

--- programmers/other/131113_조건별로_분류하여_주문상태_출력하기.py
+++ programmers/other/131113_조건별로_분류하여_주문상태_출력하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131113
 # 작성자: 김선영
-# 작성일: 2026. 01. 19. 09:42:35
+# 작성일: 2026. 01. 19. 09:45:29
 
 SELECT 
     ORDER_ID
