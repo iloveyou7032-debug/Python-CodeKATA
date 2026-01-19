88edf21955084bef3b9e260a0f1fab65b9d5f076
Sunyeong Kim <iloveyou7032@gmail.com>
2026-01-19 13:29:08 +0900

    [프로그래머스] L0 - 양꼬치 (#10)

--- programmers/L0_기초입문/입문/010_양꼬치.py
+++ programmers/L0_기초입문/입문/010_양꼬치.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120830
 # 알고리즘: 기초
 # 작성자: 김선영
-# 작성일: 2026. 01. 19. 11:44:23
+# 작성일: 2026. 01. 19. 13:29:08
 
 def solution(n, k):
     service = int(n // 10)
