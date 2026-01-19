17a4b8d1191079328b143524253a0eb8d162aa85
Sunyeong Kim <iloveyou7032@gmail.com>
2026-01-19 10:53:23 +0900

    [프로그래머스] L0 - 숫자 비교하기 (#2)

---
+++ programmers/L0_기초입문/입문/002_숫자_비교하기.py
@@ -0,0 +1,16 @@
+# 숫자 비교하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120807
+# 알고리즘: 기초
+# 작성자: 김선영
+# 작성일: 2026. 01. 19. 10:53:22
+
+def solution(num1, num2):
+    answer = 0
+    return answer
+
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1
