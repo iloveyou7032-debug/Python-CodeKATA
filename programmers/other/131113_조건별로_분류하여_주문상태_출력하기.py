900ae74bf5ae4c402bd0b1d2170c3afed4fb4205
Sunyeong Kim <iloveyou7032@gmail.com>
2026-01-19 09:42:35 +0900

    [프로그래머스] 조건별로 분류하여 주문상태 출력하기 풀이 제출
    - 문제 ID: 131113
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 09:42:35
    - 작성자: 김선영

---
+++ programmers/other/131113_조건별로_분류하여_주문상태_출력하기.py
@@ -0,0 +1,16 @@
+# 조건별로 분류하여 주문상태 출력하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131113
+# 작성자: 김선영
+# 작성일: 2026. 01. 19. 09:42:35
+
+SELECT 
+    ORDER_ID
+    , PRODUCT_ID
+    , date_format(OUT_DATE, "%Y-%m-%d") as OUT_DATE
+    , case when OUT_DATE <= "2022-05-01" then "출고완료"
+           when OUT_DATE > "2022-05-01" then "출고대기"
+           else "출고미정"
+           END as "출고여부"
+from FOOD_ORDER
+order by ORDER_ID;
